688e6cde10110d61a79e8303baafe08195c5ee99
CooleKoeGames <133784369+CooleKoeGames@users.noreply.github.com>
2023-05-27 12:12:26 +0200

    Collectibles and counting system

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int foodCollected = 0;
+
+    [SerializeField] private Text foodText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Food"))
+        {
+            Destroy(collision.gameObject);
+            foodCollected++;
+            foodText.text = "Food Collected: " + foodCollected + "/5";
+        }
+    }
+}
