1abda6a09ec795a0d627d740921266d081cade25
CooleKoeGames <133784369+CooleKoeGames@users.noreply.github.com>
2023-05-31 11:38:01 +0200

    Health system

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private Rigidbody2D rb;
+    private bool dead;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            anim.SetTrigger("hurt");
+        }
+        else
+        {
+            if (!dead)
+            {
+                anim.SetTrigger("death");
+                GetComponent<PlayerMovement>().enabled = false;
+                rb.bodyType = RigidbodyType2D.Static;
+                dead = true;
+            }
+        }
+    }
+
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
