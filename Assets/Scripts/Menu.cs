42f59381d40e0c01e5084a47706bae210379378a
CooleKoeGames <133784369+CooleKoeGames@users.noreply.github.com>
2023-06-01 19:47:00 +0200

    Added UI

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartMenu : MonoBehaviour
+public class Menu : MonoBehaviour
 {
-    public void StartGame()
+    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMain()
+    {
+        SceneManager.LoadScene(0);
     }
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private float currentTime = 0f;
+    private float startingTime = 60f;
+
+    [SerializeField] Text countdownText;
+
+    private void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    private void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+}
